d95a984c5f6e3c4d8b02a2c596032181b5714062
Prathameshkale <kaleprathmesh440@gmail.com>
2025-08-01 18:38:04 +0530

    Update first.java

--- first.java
+++ first.java
@@ -4,5 +4,6 @@ public class first {
         System.out.println("hii");
         System.out.printlin("prathamesh");
         //hiiiii
+        System.out.println("hii ritesh and krushna");
     }
 }
